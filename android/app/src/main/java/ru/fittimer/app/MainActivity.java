72978ffdfc320b7d8f861d076f8e4ff12696f8b4
edkiy73 <edkiy73@gmail.com>
2026-09-23 02:58:34 +0800

    Respect Android system font scale

--- android/app/src/main/java/ru/fittimer/app/MainActivity.java
+++ android/app/src/main/java/ru/fittimer/app/MainActivity.java
@@ -1,6 +1,6 @@
 package ru.fittimer.app;
 
-import android.os.Bundle;
+import android.os.Bundle;\nimport android.content.res.Configuration;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -9,5 +9,19 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(FitAudioPlugin.class);
         registerPlugin(FitSystemPlugin.class);
         super.onCreate(savedInstanceState);
+        applySystemFontScale();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        applySystemFontScale();
+    }
+
+    private void applySystemFontScale() {
+        if (getBridge() == null || getBridge().getWebView() == null) return;
+        float scale = getResources().getConfiguration().fontScale;
+        int zoom = Math.max(85, Math.min(200, Math.round(scale * 100f)));
+        getBridge().getWebView().getSettings().setTextZoom(zoom);
     }
 }
